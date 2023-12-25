239739655d1e5f2ac4c2b7505a623f5361de2f0d
kerimrozajac <130725852+kerimrozajac@users.noreply.github.com>
2023-12-25 01:12:25 +0100

    initial commit registration form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login/" element={<div>Login</div>} />
+      <Route path="/register/" element={<Registration />} />
+    </Routes>
   );
 }
 
---
+++ src/components/forms/RegistrationForm.js
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function RegistrationForm() {
+  const navigate = useNavigate();
+
+  // Initialize state for form fields
+  const [form, setForm] = useState({
+    first_name: "",
+    last_name: "",
+    username: "",
+    email: "",
+    password1: "",
+    password2: "",
+  });
+
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const registrationForm = event.currentTarget;
+    if (registrationForm.checkValidity() === false) {
+      event.stopPropagation();
+    }
+    setValidated(true);
+
+    const data = {
+      username: form.username,
+      password1: form.password1,
+      password2: form.password2,
+      email: form.email,
+      first_name: form.first_name,
+      last_name: form.last_name,
+      bio: form.bio,
+    };
+
+    axios
+      .post("http://localhost:8000/api/v1/auth/register/", data)
+      .then((res) => {
+        // Registering the account and tokens in the
+        // store
+        localStorage.setItem("auth", JSON.stringify({
+          access: res.data.access,
+          refresh: res.data.refresh,
+          user: res.data.user,
+        }));
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.message) {
+          setError(err.request.response);
+        }
+      });
+  };
+
+  return (
+    <Form
+      id="registration-form"
+      className="border p-4 rounded"
+      noValidate
+      validated={validated}
+      onSubmit={handleSubmit}
+    >
+      {/* Form fields go here */}
+      <Form.Group className="mb-3">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control
+          value={form.first_name}
+          onChange={(e) => setForm({ ...form, first_name: e.target.value })}
+          required
+          type="text"
+          placeholder="Enter first name"
+        />
+        <Form.Control.Feedback type="invalid">
+          This field is required.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Last name</Form.Label>
+        <Form.Control
+          value={form.last_name}
+          onChange={(e) => setForm({ ...form, last_name: e.target.value })}
+          required
+          type="text"
+          placeholder="Enter last name"
+        />
+        <Form.Control.Feedback type="invalid">
+          This field is required.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          value={form.username}
+          onChange={(e) => setForm({ ...form, username: e.target.value })}
+          required
+          type="text"
+          placeholder="Enter username"
+        />
+        <Form.Control.Feedback type="invalid">
+          This field is required.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          required
+          type="email"
+          placeholder="Enter email"
+        />
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid email.
+        </Form.Control.Feedback>
+      </Form.Group>
+      
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          value={form.password1}
+          minLength="8"
+          onChange={(e) => setForm({ ...form, password1: e.target.value })}
+          required
+          type="password"
+          placeholder="Password"
+        />
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid password.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Retype Password</Form.Label>
+        <Form.Control
+          value={form.password2}
+          minLength="8"
+          onChange={(e) => setForm({ ...form, password2: e.target.value })}
+          required
+          type="password"
+          placeholder="Retype Password"
+        />
+        <Form.Control.Feedback type="invalid">
+          Passwords do not match.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <div className="text-content text-danger">
+        {error && <p>{error}</p>}
+      </div>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default RegistrationForm;
